6a0775866d29c28f84ef914f87b6fb09d68705bb
xaviguasch <xaviguasch@gmail.com>
2020-11-02 15:06:01 +0100

    solved exercise

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('testing-1')
+const franc = require('franc')
+const langs = require('langs')
+const colors = require('colors')
+
+const input = process.argv[2]
+const langCode = franc(input)
+
+if (langCode === 'und') {
+  console.log('sorry, try with more sample text!')
+} else {
+  const language = langs.where('3', langCode)
+  console.log(`Our best guess is: ${language.name}`.yellow)
+}
